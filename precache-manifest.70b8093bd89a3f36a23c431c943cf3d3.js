f7da3f6b9e41b2535cd2b91f980105879f40f5fa
Solomon <adutias@gmail.com>
2020-07-29 14:30:13 +0300

    Updates

---
+++ precache-manifest.70b8093bd89a3f36a23c431c943cf3d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9db650537d24e9fdeb94bf86cffbe25",
+    "url": "/Alifees/index.html"
+  },
+  {
+    "revision": "5137d4d52781c9cbe20f",
+    "url": "/Alifees/static/css/main.2e700dc6.chunk.css"
+  },
+  {
+    "revision": "ded35d1ca3f217156b93",
+    "url": "/Alifees/static/js/2.01fa02f7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Alifees/static/js/2.01fa02f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5137d4d52781c9cbe20f",
+    "url": "/Alifees/static/js/main.ad2ccbc8.chunk.js"
+  },
+  {
+    "revision": "d6f363836cf2bb08e781",
+    "url": "/Alifees/static/js/runtime-main.9a7ebf78.js"
+  }
+]);
